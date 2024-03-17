6c638d8c58f5cfd9572ca4f2e07d0ca115f98993
Toki Tahmid Khan <122171015+toki513@users.noreply.github.com>
2024-03-17 15:49:31 +0600

    Updated

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const input = document.getElementById("typing")
 
+function reverseString(str){
+    return str.split("").reverse().join("")
+}
+
 function check(){
     const value = input.value
-    alert(value)
+    const reverse = reverseString(value)
+    
+
+    if(value === reverse){
+        alert("P A L I N D R O M E")
+    }else{
+        alert("W R O N G !!")
+    }
+    input.value=""
+
 }
